f4e8863af89330b4988417e3900a3e745d577d38
kevin20888802 <kevin20888802@gmail.com>
2021-12-11 20:56:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,19 @@
-"""匯入套件"""
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-"""讀取辨識物件名稱"""
 classes = None
 with open('Yolo/coco.names', 'r') as f:
  classes = [line.strip() for line in f.readlines()]
 
-"""讀取預先訓練好的官方模型"""
 net = cv2.dnn.readNet('Yolo/yolov3-tiny.weights', 'Yolo/yolov3-tiny.cfg')
 #net = cv2.dnn.readNet('Yolo/yolov3-608.weights', 'Yolo/yolov3-608.cfg')
 
-"""開始偵測"""
 cap = cv2.VideoCapture(0)
 while True:
-  # 取得圖片
   ret , image = cap.read()
   #image = plt.imread('data/a.jpg')
 
-  # 辨識
   net.setInput(cv2.dnn.blobFromImage(image, 0.00412, (320,320), (0,0,0), True, crop=False))
   #net.setInput(cv2.dnn.blobFromImage(image, 0.00412, (608,608), (0,0,0), True, crop=False))
   layer_names = net.getLayerNames()
@@ -51,7 +45,7 @@ while True:
   pass
 
   indices = cv2.dnn.NMSBoxes(boxes, confidences, 0.1, 0.1)
-  # 辨識到的結果畫出來
+
   for i in indices:
       i = i[0]
       box = boxes[i]
@@ -60,10 +54,8 @@ while True:
           cv2.rectangle(image, (round(box[0]),round(box[1])), (round(box[0]+box[2]),round(box[1]+box[3])), (255, 0, 0), 2)
           cv2.putText(image, label, (round(box[0])-10,round(box[1])-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
   
-  # 顯示
   cv2.imshow("video",image)
   
-  # 如果按q就離開
   if cv2.waitKey(1) & 0xFF == ord('q'):
       break
   #plt.imshow(image)
